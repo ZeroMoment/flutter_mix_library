4a1755dfa2a8269a047ad4403ad85387c02153b5
lizhiming <lizhiming@ksjgs.com>
2019-09-17 18:05:30 +0800

    改造跳转

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,7 @@ Widget _widgetForRoute(String route) {
   switch(route) {
     case "a_routerf":
 
-      return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          body: Center(
-            child: Text(
-              'Hello from Flutter----Page  A',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.yellow,
-              ),
-            ),
-          ),
-        ),
-      );
+      return PageAWidget();
 
     case "b_routerf":
 
@@ -45,20 +31,55 @@ Widget _widgetForRoute(String route) {
       );
 
     default:
-      return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.deepOrange,
-          body: Center(
-            child: Text(
-              'Hello from Flutter----default',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.cyanAccent,
-              ),
+      return DefaultWidget();
+  }
+}
+
+class PageAWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Text(
+            'Hello from Flutter----Page  A',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.yellow,
             ),
           ),
         ),
-      );
+      ),
+    );
   }
+
+}
+
+class DefaultWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: Center(
+          child: Text(
+            'Hello from Flutter----default',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.cyanAccent,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>PageAWidget())),
+          tooltip: 'ToPageA',
+          child: Icon(Icons.arrow_forward),
+        ),
+      ),
+    );
+  }
+
 }
