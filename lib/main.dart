68baf201bf219f8abf49bd7b705f1163e9877463
lizhiming <lizhiming@ksjgs.com>
2019-09-18 18:13:21 +0800

    mixlib 生命周期监听

--- lib/main.dart
+++ lib/main.dart
@@ -61,23 +61,68 @@ class DefaultWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Center(
-          child: Text(
-            'Hello from Flutter----default',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.cyanAccent,
-            ),
+      home: DefaultPage(),
+    );
+  }
+
+}
+
+class DefaultPage extends StatefulWidget {
+  @override
+  _DefaultState createState() => _DefaultState();
+
+}
+
+class _DefaultState extends State<DefaultPage> with WidgetsBindingObserver {
+
+  var clickNum = 0;
+
+  @override
+  void initState() {
+    print("default page 创建....");
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("page statu: $state");
+  }
+
+  void _changeClickNum() {
+    setState(() {
+      ++clickNum;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: Text(
+          'default flutter page: $clickNum',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.cyanAccent,
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>PageAWidget())),
-          tooltip: 'ToPageA',
-          child: Icon(Icons.arrow_forward),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _changeClickNum();
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>PageAWidget()));
+        },
+        //onPressed: ()=>,
+        tooltip: 'ToPageA',
+        child: Icon(Icons.arrow_forward),
       ),
     );
   }
